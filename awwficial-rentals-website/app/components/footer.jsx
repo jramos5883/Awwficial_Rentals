0471eb50377558cc4caaf5f0880ef83794aec537
jramos5883 <jramos5883@gmail.com>
2023-09-21 18:04:21 -0700

    Working on the gallery, have alot down but working on centering.

--- awwficial-rentals-website/app/components/footer.jsx
+++ awwficial-rentals-website/app/components/footer.jsx
@@ -1,33 +1,64 @@
 import Link from "next/link";
-import Button from '@mui/material/Button';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import FacebookIcon from '@mui/icons-material/Facebook';
+import Button from "@mui/material/Button";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
 import { FaTiktok } from "react-icons/fa6";
 
 // Assigned to Tomomi :)
 export default function Footer() {
-  return(
+  return (
     <footer className="text-center">
       <section className="bg-yellow-50 py-10">
-        <h3 className="text-2xl leading-9 tablet:text-5xl "> Ready to Make Your Event <span  className="block">Awwficial?</span></h3>
-        <a href="/BookNow"><Button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-bold py-2 px-5"> Book now !</Button></a>
+        <h3 className="text-2xl leading-9 tablet:text-5xl ">
+          {" "}
+          Ready to Make Your Event <span className="block">Awwficial?</span>
+        </h3>
+        <a href="/BookNow">
+          <Button className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white font-bold py-2 px-5">
+            {" "}
+            Book now !
+          </Button>
+        </a>
       </section>
       <section className="bg-purple-700 py-5">
         <nav className="max-w-xs px-6 pt-6 pb-3 mx-auto">
           <ul className="list-none px-5 flex justify-around items-center">
-            <li><Link href="/FAQs" className="no-underline px-1 text-white  hover:text-pink-500">FAQs</Link></li>
-            <li><Link href="/Contact" className="no-underline px-1 text-white  hover:text-pink-500">Contact</Link></li>
+            <li>
+              <Link
+                href="/FAQs"
+                className="no-underline px-1 text-white  hover:text-pink-500"
+              >
+                FAQs
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/Contact"
+                className="no-underline px-1 text-white  hover:text-pink-500"
+              >
+                Contact
+              </Link>
+            </li>
           </ul>
         </nav>
         <div className="w-5/12 max-w-xs mx-auto">
           <ul className="list-none px-7 flex justify-around items-center">
-            <li><FacebookIcon className="p-1 text-3xl text-white hover:text-pink-500"/></li>
-            <li><InstagramIcon className="p-1 text-3xl text-white hover:text-pink-500"/></li>
-            <li><FaTiktok className="p-1 text-2xl  text-white hover:text-pink-500"></FaTiktok></li>
+            <li>
+              <FacebookIcon className="p-1 text-3xl text-white hover:text-pink-500" />
+            </li>
+            <li>
+              <InstagramIcon className="p-1 text-3xl text-white hover:text-pink-500" />
+            </li>
+            <li>
+              <FaTiktok className="p-1 text-2xl  text-white hover:text-pink-500"></FaTiktok>
+            </li>
           </ul>
         </div>
-        <p className="leading-normal  text-white">© Copyright 2023 Awwficial Rentals. <span className="block">All rights reserved.</span></p>
+        <p className="leading-normal  text-white">
+          © Copyright 2023 Awwficial Rentals.{" "}
+          <span className="block">All rights reserved.</span>
+        </p>
       </section>
     </footer>
-  ) ;
+  );
 }
--- awwficial-rentals-website/app/components/gallery.jsx
+++ awwficial-rentals-website/app/components/gallery.jsx
@@ -1,3 +1,34 @@
+import DropboxVideoEmbed from "./video.comp";
+
 export default function Gallery() {
-  return <div>Gallery Section</div>;
+  return (
+    <div className="bg-yellow-50">
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-1" />
+        <div className="col-span-3">
+          <DropboxVideoEmbed videoLink="https://www.dropbox.com/scl/fi/epradd978l20pvktd6xrc/AwwficialCC_2.mov?rlkey=j1i1gkd3ejd3x00bjjx4xk9eg&dl=0" />
+        </div>
+        <div className="col-span-1" />
+        <div className="col-span-3">
+          <DropboxVideoEmbed videoLink="https://www.dropbox.com/scl/fi/epradd978l20pvktd6xrc/AwwficialCC_2.mov?rlkey=j1i1gkd3ejd3x00bjjx4xk9eg&dl=0" />
+        </div>
+        <div className="col-span-1" />
+        <div className="col-span-3">
+          <DropboxVideoEmbed videoLink="https://www.dropbox.com/scl/fi/epradd978l20pvktd6xrc/AwwficialCC_2.mov?rlkey=j1i1gkd3ejd3x00bjjx4xk9eg&dl=0" />
+        </div>
+      </div>
+
+      <div className="grid grid-cols-12 gap-4">
+        <div className="col-span-3" />
+        <div className="col-span-3">
+          <DropboxVideoEmbed videoLink="https://www.dropbox.com/scl/fi/epradd978l20pvktd6xrc/AwwficialCC_2.mov?rlkey=j1i1gkd3ejd3x00bjjx4xk9eg&dl=0" />
+        </div>
+        <div className="" />
+        <div className="col-span-3">
+          <DropboxVideoEmbed videoLink="https://www.dropbox.com/scl/fi/epradd978l20pvktd6xrc/AwwficialCC_2.mov?rlkey=j1i1gkd3ejd3x00bjjx4xk9eg&dl=0" />
+        </div>
+        <div className="col-span-3" />
+      </div>
+    </div>
+  );
 }
