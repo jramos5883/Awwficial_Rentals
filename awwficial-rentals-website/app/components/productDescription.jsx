64beda23c51abcf15f955c79791c3b173f0da538
Tomomi-K1 <tomomi.music@gmail.com>
2023-10-10 18:19:01 -0700

    updated text and icon on productDescription

--- awwficial-rentals-website/app/components/productDescription.jsx
+++ awwficial-rentals-website/app/components/productDescription.jsx
@@ -1,13 +1,17 @@
 import DropboxVideoEmbed from "./video.comp";
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { ImCamera } from "react-icons/im";
+import { MdWifiTethering, Md360 } from "react-icons/md";
+import { FaPerson, FaRegFaceSmileBeam } from "react-icons/fa6";
+import { BsHandThumbsUp } from "react-icons/bs";
 
 export default function ProductDescription() {
   return(
     <main>
       <section className="grid grid-cols-1 justify-items-center tablet:grid-cols-2 p-2">
         <div className="flex flex-col justify-center items-center px-5 lapsm:max-w-lg laplg:max-w-xl">
-          <h2 className="text-center">Why Awwficial Photobooth?</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ipsa consectetur placeat cum officia tempore recusandae eum ratione officiis, delectus, voluptates cupiditate! Ipsa optio eius molestias impedit sunt, incidunt doloribus.</p>
+          <h2 className="text-center">Rewriting the Pose-ibilities</h2>
+          <p>Step into a world where every angle tells a story. Our 360-degree photo booth doesn't just capture moments; it creates narratives. With a spin and a snap, you're in the spotlight from all sides. It's photography reimagined, and you're the star of the show!</p>
         </div>
         <div className="m-auto">
           <img className="block w-full"  src="https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
@@ -15,37 +19,40 @@ export default function ProductDescription() {
         </div>
       </section>
       <section className="text-center py-10 px-10">
-        <h2 className="pb-5">Why a 360 Photobooth?</h2>
+        <h2 className="pb-5">Why The 360 Photo Booth?</h2>
         <div className="grid grid-cols-1 celllg:grid-cols-2 tablet:grid-cols-3 lapsm:grid-cols-6">
           <article className="text-center px-3">
-            <FavoriteBorderIcon/>
-            <h5>Great Service</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ab officia similique culpa, fuga repellendus laboriosam qui expedita.</p>
+            <ImCamera  className="text-xl"/>
+            <h5>Unforgettable Memories</h5>
+
           </article>
           <article className="text-center px-3">
-            <FavoriteBorderIcon/>
-            <h5>Great Service</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ab officia similique culpa, fuga repellendus laboriosam qui expedita.</p>
+            <MdWifiTethering className="text-2xl" />
+            <h5 className>Instantly Sharable</h5>
+      
           </article>
           <article className="text-center px-3">
-            <FavoriteBorderIcon/>
-            <h5>Great Service</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ab officia similique culpa, fuga repellendus laboriosam qui expedita.</p>
+          <FavoriteBorderIcon />
+            <h5>Elevate Your Event</h5>
+
           </article>
           <article className="text-center px-3">
-            <FavoriteBorderIcon/>
-            <h5>Great Service</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ab officia similique culpa, fuga repellendus laboriosam qui expedita.</p>
+          <div className="flex flex-col m-0 items-center">
+              <FaPerson className="text-xl"/>
+              <Md360 className="text-xl"/>
+            </div>
+            <h5 className="mt-3">Capture Every Angle</h5>
+
           </article>
           <article className="text-center px-3">
-            <FavoriteBorderIcon/>
-            <h5>Great Service</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ab officia similique culpa, fuga repellendus laboriosam qui expedita.</p>
+            <FaRegFaceSmileBeam className="text-xl"/>
+            <h5>Fun & Interactive</h5>
+
           </article>
           <article className="text-center px-3">
-            <FavoriteBorderIcon/>
-            <h5>Great Service</h5>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat ab officia similique culpa, fuga repellendus laboriosam qui expedita.</p>
+            <BsHandThumbsUp className="text-xl"/>
+            <h5>Hassle Free</h5>
+    
           </article>
         </div>
       </section>
@@ -55,8 +62,9 @@ export default function ProductDescription() {
         </div> */}
         <img className="block w-full"  src="https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="" />
         <div className="flex flex-col justify-center items-center px-5 lapsm:max-w-lg laplg:max-w-xl">
-          <h2 className="text-center">Why Awwficial Photobooth?</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis ipsa consectetur placeat cum officia tempore recusandae eum ratione officiis, delectus, voluptates cupiditate! Ipsa optio eius molestias impedit sunt, incidunt doloribus.</p>
+          <h2 className="text-center"> Turn Down For What?!
+          </h2>
+          <p>Our 360-degree photo booth is the ultimate crowd-pleaser. Elevate the fun, capture every angle, and create unforgettable memories. Whether it's a wedding, corporate gala, or birthday bash, our photo booth adds a unique touch of excitement!</p>
         </div>
        
       </section>
