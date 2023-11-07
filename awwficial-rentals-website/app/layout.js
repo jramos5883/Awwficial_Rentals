cdf290b9ebdc41ed35259802d052df4708e32c1d
JeffLi117 <li.jeffreyt@yahoo.com>
2023-11-07 09:00:38 -0600

    Add FAQ questions to FAQs display page by mapping over array of objects; use Accordion from Material UI; change title in metadata on layout page to match Awwficial Rentals

--- awwficial-rentals-website/app/layout.js
+++ awwficial-rentals-website/app/layout.js
@@ -7,7 +7,7 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Afficial Rentals",
+  title: "Awwficial Rentals",
   description: "360 Photo Booth Rental Company",
 };
 
