31ba3559ccfd947a8e081ba85487c0412887b0f8
JeffLi117 <li.jeffreyt@yahoo.com>
2023-12-06 13:36:32 -0600

    Retest SendGrid with new API key and delete .env

--- awwficial-rentals-website/app/api/send-mail/route.js
+++ awwficial-rentals-website/app/api/send-mail/route.js
@@ -8,12 +8,12 @@ export async function GET(Request) {
     const apiKey = process.env.SENDGRID_API_KEY;
     sgMail.setApiKey(apiKey);
     const msg = {
-        to: 'INSERT_EMAIL_HERE',
-        from: 'INSERT_VERIFIED_SENDGRID_EMAIL_HERE', // Use the email address or domain you verified above
+        to: 'INSERT_OWNER_EMAIL',
+        from: 'INSERT_VERIFIED_AWWFICIALRENTALS_EMAIL', // Use the email address or domain you verified above
         subject: 'Sending with Twilio SendGrid is Fun by Tomomi for Test',
         html: '<p>this is test to see if SendGrid is working</p><strong>and easy to do anywhere, even with Node.js</strong>',
     };
-    msg.to = "INSERT_EMAIL_HERE";
+    msg.to = "INSERT_OWNER_EMAIL";
     console.log(msg);
     sgMail
         .send(msg)
