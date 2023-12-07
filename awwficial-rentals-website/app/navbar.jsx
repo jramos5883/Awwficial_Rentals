f3f1572facaa3eb5bfacc12ac58e1f2193d1dc8a
Oscar Carvente <oscar.carvente@gmail.com>
2023-12-06 21:58:35 -0800

    Adjust button styling in navbar

--- awwficial-rentals-website/app/navbar.jsx
+++ awwficial-rentals-website/app/navbar.jsx
@@ -23,7 +23,7 @@ export default function Navbar() {
     // I plan to make the navbar transparent later,
     // but I do like seeing the colors change with the breakpoints.
     // Helps me with responsive design.
-    <div className="flex flex-row items-center justify-between bg-[#EDE9FD]">
+    <div className="flex flex-row items-center justify-between bg-[#EDE9FD] h-[112px]">
       <Image
         src='/images/navbarLogo.png'
         width={194}
