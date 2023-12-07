e0b3f3c0a587b7b5cd58a768949bb4719b6b8118
Oscar Carvente <oscar.carvente@gmail.com>
2023-12-06 16:58:36 -0800

    Include lato font family

--- awwficial-rentals-website/app/components/LargeButton.jsx
+++ awwficial-rentals-website/app/components/LargeButton.jsx
@@ -1,9 +1,9 @@
 import Link from 'next/link'
-
+import {lato} from '../ui/fonts.jsx';
 export default function LargeButton ({name, href}) {
   return (
     <Link href={href}>
-      <button className='flex px-[18px] py-3.5 rounded-lg	bg-primary text-white'>
+      <button className='flex px-[18px] py-3.5 rounded-lg	bg-primary text-white ' style={{ fontFamily: lato }}>
         {name}
       </button>
     </Link>
