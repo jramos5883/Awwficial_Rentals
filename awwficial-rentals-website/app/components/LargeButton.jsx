e0a6b6c6f58a48217b6f8a7268533d4ed56ab66a
Oscar Carvente <oscar.carvente@gmail.com>
2023-12-08 17:47:55 -0800

    changes color on hover over large button

--- awwficial-rentals-website/app/components/LargeButton.jsx
+++ awwficial-rentals-website/app/components/LargeButton.jsx
@@ -3,7 +3,7 @@ import {lato} from '../ui/fonts.jsx';
 export default function LargeButton ({name, href}) {
   return (
     <Link href={href}>
-      <button className='flex px-[18px] py-3.5 rounded-lg	bg-primary text-white text-xl leading-7 	not-italic' style={{ fontFamily: lato.className }}>
+      <button className='flex px-[18px] py-3.5 rounded-lg hover:bg-[#541D59]	bg-primary text-white text-xl leading-7 	not-italic' style={{ fontFamily: lato.className }}>
         {name}
       </button>
     </Link>
