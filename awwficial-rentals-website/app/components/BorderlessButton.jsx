86e002a32f9dce0764e5ea0b4e9f46d3b1f67fa9
Oscar Carvente <oscar.carvente@gmail.com>
2023-12-12 21:01:15 -0800

    adds active styling to borderless button

--- awwficial-rentals-website/app/components/BorderlessButton.jsx
+++ awwficial-rentals-website/app/components/BorderlessButton.jsx
@@ -3,7 +3,7 @@ import {lato} from '../ui/fonts.jsx';
 export default function BorderlessButton ({name, href}) {
   return (
     <Link href={href}>
-      <button className='h-auto w-auto flex flex-col px-[18px] py-3.5 items-center text-primary border-2 border-transparent hover:border-primary-hover rounded-lg text-xl leading-7 font-semibold	not-italic ' style={{fontFamily: lato}}>{name}</button>
+      <button className='h-auto w-auto flex flex-col px-[18px] py-3.5 items-center text-primary border-2 border-transparent hover:border-primary-hover rounded-lg text-xl leading-7 font-semibold	not-italic active:border-primary-active active:text-primary-active' style={{fontFamily: lato}}>{name}</button>
     </Link>
   )
 }
