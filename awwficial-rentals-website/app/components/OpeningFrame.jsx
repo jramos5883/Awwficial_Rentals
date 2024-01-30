51a21e94b27a063a7587eb783f672538211e43c7
Tomomi-K1 <tomomi.music@gmail.com>
2024-01-29 17:49:37 -0800

    removed z-index on OpenFrame

--- awwficial-rentals-website/app/components/OpeningFrame.jsx
+++ awwficial-rentals-website/app/components/OpeningFrame.jsx
@@ -6,7 +6,7 @@ import DropboxVideoEmbed from "./video.comp";
 export default function OpeningFrame() {
 
   return (
-    <div className="min-h-min justify-center items-center relative -z-10">
+    <div className="min-h-min justify-center items-center relative">
         <DropboxVideoEmbed videoLink="https://www.dropbox.com/scl/fi/dhfgohtdz00h0c6ne5gga/Awwficial-Rentals-Promo-Video.mp4?rlkey=jv3hk10zb2ywcqt7lkmr4680l&dl=0" />
       <div className='w-full h-full absolute top-0 left-0 flex flex-col px-10 justify-center items-center'>
         <p className={`text-white text-center font-extrabold text-2xl tablet:text-4xl mb-3 ${playfairDisplay.className}`}>
